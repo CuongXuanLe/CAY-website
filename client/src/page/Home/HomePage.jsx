67fa55e22ccab3d748ab9e32dd8192adbc5fd709
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-11-23 13:14:22 +0700

    done: create client

---
+++ client/src/page/Home/HomePage.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <div className="text-5xl text-slate-700 text-center mt-96">
+            test React + tailwindcss hehe
+        </div>
+    )
+}
+
+export default Home
