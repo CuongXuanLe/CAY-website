1bceffd71cd71b9624f37e7a27dc630bd9782403
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-11-23 13:17:11 +0700

    done: create admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import AdminPage from './page/AdminPage/AdminPage';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Routes>
+        <Route path="/" element={<AdminPage/>}/>      
+      </Routes>
+    </Router>
   );
 }
 
---
+++ admin/src/page/AdminPage/AdminPage.jsx
@@ -0,0 +1,9 @@
+const AdminPage = () => {
+    return(
+        <div className="text-center text-5xl mt-96">
+            AdminPage react + taiwindcss 
+        </div>
+    )
+}
+
+export default AdminPage
