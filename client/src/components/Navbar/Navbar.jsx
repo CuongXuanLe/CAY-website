863da42bd47af48b0868fe4b24025663dc5bb5c8
Khaug <huynhkhang690@gmail.com>
2022-11-29 00:12:38 +0700

    Feat: Responsive the navigation bar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import images from "../../contants/images";
-
-
+import { GiHamburgerMenu } from "react-icons/gi"
 
 const Navbar = () => {
+    const [open,setOpen] = useState(false);
+
     return(
-        <div className="flex justify-between bg-inherit w-full h-28 pt-8 px-9" >
+        <div className="md:flex justify-between bg-inherit w-full h-28 md:pt-8 md:px-9 sm:pt-5 sm:px-5">
+
             <div className="flex h-full">
-                <div className="w-20 h-20 border border-black ">
+                <div className="md:w-20 md:h-20 border border-black sm:w-14 sm:h-14">
                     <img src={images.Logo} alt="logo" className="w-full h-full"/>
                 </div>
-                <div className="flex flex-col h-[80px] ml-2 mt-0.5 justify-end font-medium tracking-[.2em]">
+                <div className="flex flex-col h-[80%] ml-2 mt-0.5 md:justify-end font-medium tracking-[.2em] sm:mt-3">
                     <span>CAY</span>  
                     <span>STUDIO</span>
                 </div>
             </div>
+            <div className="md:hidden absolute top-8 right-6" onClick={() => setOpen(!open)}>
+                <GiHamburgerMenu className="w-8 h-8" />
+            </div>
 
             <div className="h-full">
-                <ul className="flex font-normal h-3/4 tracking-[.2em]">
-                    <li className="mx-6 my-auto hover:border-b-2 hover:border-navybrown"><NavLink to="/home" ><span className="pl-1">HOME</span></NavLink></li>
-                    <li className="mx-6 my-auto hover:border-b-2 hover:border-navybrown"><NavLink to="/guide"><span className="pl-1">GUIDE</span></NavLink></li>
-                    <li className="mx-6 my-auto hover:border-b-2 hover:border-navybrown"><NavLink to="/schedule"><span className="pl-1">SCHEDULE</span></NavLink></li>
-                    <li className="mx-6 my-auto hover:border-b-2 hover:border-navybrown"><NavLink to="/portfolio"><span className="pl-1">PORTFOLIO</span></NavLink></li>
-                    <li className="mx-6 my-auto hover:border-b-2 hover:border-navybrown"><NavLink to="/contact"><span className="pl-1">CONTACT</span></NavLink></li> 
+                <ul className={`md:flex font-normal h-3/4 tracking-[.2em] absolute md:static transition-all duration-500 ease-in ${open ? '' : 'top-[-490px]'}`}>
+                    <li className="md:mx-6 md:my-auto sm:my-5"><NavLink to="/home" ><span className="pl-1 hover:border-b-2 hover:border-navybrown">HOME</span></NavLink></li>
+                    <li className="md:mx-6 md:my-auto sm:my-5"><NavLink to="/guide"><span className="pl-1 hover:border-b-2 hover:border-navybrown">GUIDE</span></NavLink></li>
+                    <li className="md:mx-6 md:my-auto sm:my-5"><NavLink to="/schedule"><span className="pl-1 hover:border-b-2 hover:border-navybrown">SCHEDULE</span></NavLink></li>
+                    <li className="md:mx-6 md:my-auto sm:my-5"><NavLink to="/portfolio"><span className="pl-1 hover:border-b-2 hover:border-navybrown">PORTFOLIO</span></NavLink></li>
+                    <li className="md:mx-6 md:my-auto sm:my-5"><NavLink to="/contact"><span className="pl-1 hover:border-b-2 hover:border-navybrown">CONTACT</span></NavLink></li> 
                 </ul>
             </div>
+
         </div>
     )
 }
