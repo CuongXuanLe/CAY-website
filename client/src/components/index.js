b0880d23718c0ee10c48e2044af3367e2310ebbd
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-12-01 19:57:27 +0700

    solved: conflict

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,5 @@
 import Navbar from "./Navbar/Navbar";
-import Footer from "./Footer/Footer";
 import Slider from "./Home/Slider/Slider";
 import Blog from "./Home/Blog/Blog";
 
-export { Navbar, Footer, Slider, Blog };
+export { Navbar, Slider, Blog };
