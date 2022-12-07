d80ed09b0d2ccfe8afbecd0779d2a806338727ec
ppn243 <nguyenwin2432002@gmail.com>
2022-12-07 23:54:13 +0700

    Update test api header and footer

---
+++ backend/database/seeders/vp_admin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class vp_admin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admin')->insert([
+            [
+                'name' => "admin",
+                'password'=> Hash::make('admin'),
+            ],
+            
+        ]);
+    }
+}
