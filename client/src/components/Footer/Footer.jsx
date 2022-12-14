95680cab4ea1b48a0bf1bcd74eef16b890417627
Khaug <huynhkhang690@gmail.com>
2022-12-14 12:15:10 +0700

    [Footer][3xcmxgc] centered footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,48 +5,51 @@ import { SiInstagram } from "react-icons/si";
 import { SiGmail } from "react-icons/si";
 
 const Footer = () => {
-
   //getAPI
   const [footercontents, setFootercontents] = useState([]);
   const apiFooter = "http://127.0.0.1:8000/api/get-footer";
   useEffect(() => {
     async function getFooterData() {
       const res = await axios.get(apiFooter);
-      setFootercontents(res.data.data)
+      setFootercontents(res.data.data);
+      console.log(res.data.data);
     }
     getFooterData();
   }, []);
 
   return (
     <>
-      <div className="mt-4 border-t-[2px] border-[#E0E0E0]">
+      <div className="mt-4 w-full border-t-[2px] border-[#E0E0E0]">
         <div className="grid h-24 grid-cols-4 items-center">
-          <div>
-            {footercontents.map((footercontent) => (
-              <h2 className="ml-2 font-lato font-semibold">
-                {footercontent.name}
-              </h2>
-            ))}
+          <div className="mx-auto w-[60%]">
+            {footercontents &&
+              footercontents.map((footercontent) => (
+                <h2 className="ml-2 font-lato font-semibold">
+                  {footercontent.name}
+                </h2>
+              ))}
           </div>
-          <div>
-            {footercontents.map((footercontent) => (
-              <p
-                key={footercontent.id}
-                className="w-[16rem] text-sm font-normal"
-              >
-                {footercontent.address}
-              </p>
-            ))}
+          <div className="mx-auto w-[60%]">
+            {footercontents &&
+              footercontents.map((footercontent) => (
+                <p
+                  key={footercontent.id}
+                  className="w-[16rem] text-sm font-normal"
+                >
+                  {footercontent.address}
+                </p>
+              ))}
           </div>
-          <div>
-            {footercontents.map((footercontent) => (
-              <p key={footercontent.id} className="text-sm font-normal">
-                {footercontent.phone_number} <br />
-                {footercontent.email}
-              </p>
-            ))}
+          <div className="mx-auto w-[60%]">
+            {footercontents &&
+              footercontents.map((footercontent) => (
+                <p key={footercontent.id} className="text-sm font-normal">
+                  {footercontent.phone_number} <br />
+                  {footercontent.email}
+                </p>
+              ))}
           </div>
-          <div className="flex ">
+          <div className="mx-auto flex w-[60%]">
             <div className="group mx-2 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border-[1px] border-black duration-500 ease-in-out hover:bg-black">
               <a href="https://www.facebook.com/CAYYYYYYYY.studio">
                 <FaFacebookF className="duration-500 ease-in-out group-hover:text-white" />
@@ -63,7 +66,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="ml-2">
-          <p className="font-lato text-sm ">
+          <p className="mx-auto w-[90%] font-lato text-sm">
             2022 All rights reserved Development by CAY studio
           </p>
         </div>
--- client/src/page/Home/HomePage.jsx
+++ client/src/page/Home/HomePage.jsx
@@ -1,9 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Slider, Blog, Footer, Sidebar } from "../../components";
+import { Slider, Blog } from "../../components";
 
 const Home = () => {
-
   //getAPI
   const [contents, setContents] = useState([]);
   const apiHome = "http://127.0.0.1:8000/api/get-home";
@@ -11,6 +10,7 @@ const Home = () => {
     async function getDataHome() {
       const res = await axios.get(apiHome);
       setContents(res.data.data);
+      // console.log(res.data.data);
     }
     getDataHome();
   }, []);
@@ -19,50 +19,55 @@ const Home = () => {
     <>
       <div className="snap-y snap-mandatory">
         <div className="relative h-screen w-full snap-end">
-          {contents.map((content) => (
-            <img
-              key={content.id}
-              src={content.background1}
-              alt="background1"
-              className="z-0 h-full w-full object-cover"
-            ></img>
-          ))}
+          {contents &&
+            contents.map((content) => (
+              <img
+                key={content.id}
+                src={content.background1}
+                alt="background1"
+                className="z-0 h-full w-full object-cover"
+              ></img>
+            ))}
           <div className="absolute bottom-7 left-7">
             <h1 className="text-3xl font-thin">
-              {contents.map((content) => (
-                <span key={content.id} className="font-semibold">
-                  {content.title1}
-                </span>
-              ))}
+              {contents &&
+                contents.map((content) => (
+                  <span key={content.id} className="font-semibold">
+                    {content.title1}
+                  </span>
+                ))}
               <br />
-              {contents.map((content) => (
-                <h2
-                  key={content.id}
-                  className="text-[18px] font-normal tracking-wider"
-                >
-                  {content.subtitle1}
-                </h2>
-              ))}
+              {contents &&
+                contents.map((content) => (
+                  <h2
+                    key={content.id}
+                    className="text-[18px] font-normal tracking-wider"
+                  >
+                    {content.subtitle1}
+                  </h2>
+                ))}
             </h1>
-            {contents.map((content) => (
-              <p key={content.id} className="w-[55%]">
-                {content.description1}
-              </p>
-            ))}
+            {contents &&
+              contents.map((content) => (
+                <p key={content.id} className="w-[55%]">
+                  {content.description1}
+                </p>
+              ))}
           </div>
         </div>
         {/* EndSection1 */}
 
         {/* StartSection2 */}
         <div className="relative h-[calc(100vh-6rem)] w-full snap-end">
-          {contents.map((content) => (
-            <img
-              key={content.id}
-              src={content.background2}
-              alt="background2"
-              className="absolute top-0 left-0 -z-20 h-full w-full object-cover"
-            ></img>
-          ))}
+          {contents &&
+            contents.map((content) => (
+              <img
+                key={content.id}
+                src={content.background2}
+                alt="background2"
+                className="absolute top-0 left-0 -z-20 h-full w-full object-cover"
+              ></img>
+            ))}
           <div className="absolute top-0 left-0 -z-10 h-full w-full backdrop-blur-[5px]"></div>
           <div className="flex h-full flex-col items-center justify-center">
             <div className="flex h-72 w-232">
@@ -83,49 +88,47 @@ const Home = () => {
                 </div>
               </div>
             </div>
-            {contents.map((content) => (
-              <h2
-                key={content.id}
-                className="mt-12 w-[50rem] text-center font-playfair text-2xl"
-              >
-                {content.description2}
-              </h2>
-            ))}
+            {contents &&
+              contents.map((content) => (
+                <h2
+                  key={content.id}
+                  className="mt-12 w-[50rem] text-center font-playfair text-2xl"
+                >
+                  {content.description2}
+                </h2>
+              ))}
           </div>
         </div>
         {/* EndSection2 */}
 
         {/* StartSection3 */}
-        <div className="h-[calc(100vh-6rem)] w-full snap-end bg-white">
+        <div className="h-[calc(100vh-14rem)] w-full snap-end bg-white">
           <div className="grid h-3/4 grid-cols-2">
             <div className="w-full">
-              {contents.map((content) => (
-                <img
-                  key={content.id}
-                  className="h-96 w-full object-cover"
-                  src={content.imgAbout}
-                  alt=""
-                ></img>
-              ))}
+              {contents &&
+                contents.map((content) => (
+                  <img
+                    key={content.id}
+                    className="h-96 w-full object-cover"
+                    src={content.imgAbout}
+                    alt=""
+                  ></img>
+                ))}
             </div>
             <div className="flex h-96 w-full flex-col items-center justify-center text-center">
               <h2 className=" mb-5 text-xl font-semibold tracking-[0.2em]">
                 ABOUT US
               </h2>
-              {contents.map((content) => (
-                <p key={content.id} className="px-10 font-lato">
-                  {content.descriptionAbout}
-                </p>
-              ))}
+              {contents &&
+                contents.map((content) => (
+                  <p key={content.id} className="px-10 font-lato">
+                    {content.descriptionAbout}
+                  </p>
+                ))}
             </div>
-          </div>
-
-          <div className="w-full">
-            <Footer />
           </div>
         </div>
         {/* EndSection3 */}
-        <Sidebar />
       </div>
     </>
   );
