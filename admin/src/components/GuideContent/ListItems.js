6e8fe7dd7d005c6113d4dcc560c69b494e96b537
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-12-15 09:54:19 +0700

    add: delete func for list-item

--- admin/src/components/GuideContent/ListItems.js
+++ admin/src/components/GuideContent/ListItems.js
@@ -1,11 +1,23 @@
 import { BsPencil } from "react-icons/bs";
 import { AiOutlineDelete } from "react-icons/ai";
+import axios from "axios"
 
-const ListItems = ({tools}) => {
-
-    
-
-
+const ListItems = ({tools, index}) => {
+    // const editProduct = (id) => {
+    //     naviga
+    // }
+    const deleteItem = async(id) => {
+        // const url = "`http://127.0.0.1:8000/api/delete-items/${id}`" 
+        const res = await axios.delete(`http://127.0.0.1:8000/api/delete-items/${id}`)
+        .then(({result})=> {
+            console.log('success')
+            alert('delete success')
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+        window.location.reload(false);
+    } 
     return(
         <div className="flex justify-between items-center border-2 border-black px-2 py-1 rounded mb-2">
             <p>{tools}</p>
@@ -13,7 +25,7 @@ const ListItems = ({tools}) => {
                 <button className="hover:bg-[#a7705c] hover:text-white p-2 rounded">
                     <BsPencil />
                 </button>
-                <button className="text-red-600 hover:bg-red-600 hover:text-white p-2 rounded">
+                <button onClick={() => deleteItem(index)} className="text-red-600 hover:bg-red-600 hover:text-white p-2 rounded">
                     <AiOutlineDelete/>
                 </button>
             </div>
