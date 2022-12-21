3217d845863c84edb515a0acdd57f88926c4b259
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-12-21 16:47:46 +0700

    update: implement UI of portfolio

--- client/src/page/Portfolio/Portfolio.js
+++ client/src/page/Portfolio/Portfolio.js
@@ -1,19 +1,14 @@
-// import { Link } from "react-router-dom"
 import { data_list } from "../../contants";
+import { Routes, Route } from "react-router-dom";
+import { PortfolioContent } from "../../components";
 
 const Portfolio = () => {
     return (
         <div className="relative mx-auto mb-20 w-4/5 pt-32">
-            <div className="main_page">
-                {
-                    data_list.map((item,index=0) => (
-                        <div className="post_box" key={item.key}>
-                            <img src={item.img} alt="" />
-                            {/* <h2>{item.tagline}</h2> */}
-                        </div>
-                    ))
-                }
-      </div>
+            <Routes>
+                <Route exact path="/" element={<PortfolioContent/>} />
+                <Route exact path="/albumDetails" element={""} />
+            </Routes>
         </div>
     )
 }
