5310be65ee8193630070b8745bef8318d6d6a1df
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-12-21 19:55:32 +0700

    done: implementUI-Portfolio

--- client/src/page/Portfolio/Portfolio.js
+++ client/src/page/Portfolio/Portfolio.js
@@ -1,13 +1,13 @@
 import { data_list } from "../../contants";
 import { Routes, Route } from "react-router-dom";
-import { PortfolioContent } from "../../components";
+import { PortfolioContent, PortfolioDetails } from "../../components";
 
 const Portfolio = () => {
     return (
         <div className="relative mx-auto mb-20 w-4/5 pt-32">
             <Routes>
                 <Route exact path="/" element={<PortfolioContent/>} />
-                <Route exact path="/albumDetails" element={""} />
+                <Route path="/albumDetails" element={<PortfolioDetails/>} />
             </Routes>
         </div>
     )
