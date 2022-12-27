111697c06d6cc7b5c9d61ba8f7c5c5c55f103e2b
ppn243 <nguyenwin2432002@gmail.com>
2022-12-27 22:02:43 +0700

    Fix Bug

--- backend/app/Models/Image.php
+++ backend/app/Models/Image.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Portfolio;
 
 class Image extends Model
 {
     use HasFactory;
+
+    protected $table = 'images';
+
+    protected $fillable = [
+        'image',
+        'portfolio_id',
+    ];
+
+    public function portfolio()
+    {
+        return $this->belongsTo(Portfolio::class);
+    }
 }
--- backend/database/migrations/2022_12_23_135648_create_portfolio_table.php
+++ backend/database/migrations/2022_12_23_135648_create_portfolio_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('images', function (Blueprint $table) {
+        Schema::create('portfolio', function (Blueprint $table) {
             $table->id();
+            $table->string('name_album');
+            $table->string('category');
+            $table->text('thumbnails');
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('images');
+        Schema::dropIfExists('portfolio');
     }
 };
