da653419144fe411383f9bc40666c560a72ed235
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-12-27 20:04:07 +0700

    handle: api

--- client/src/page/Schedule/ScheduleBooking.jsx
+++ client/src/page/Schedule/ScheduleBooking.jsx
@@ -1,61 +1,131 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { BookingSuccess, SmallCalendar } from "../../components";
+import "flatpickr/dist/flatpickr.css";
+import Flatpickr from "react-flatpickr";
 
+import axios from "axios";
 const ScheduleBooking = () => {
+    const [title, getTitle] = useState();
+    const [birthday, getBirthday] = useState();
+    const [phone, getPhone] = useState();
+    const [email, getEmail] = useState();
+    const [note, getNote] = useState();
+    const [date, getDate] = useState();
+    const [startTime, getTimeStart] = useState();
+    const [endTime, getTimeEnd] = useState();
+
+    const CreateBooking = async (e) => {
+        e.preventDefault();
+
+        const url = "http://127.0.0.1:8000/api/schedule";
+        const formData = {
+            title: title,
+            phone: phone,
+            birthday: birthday,
+            email: email,
+            note: note,
+            created_at: date,
+            start: startTime,
+            end: endTime,
+        }
+        await axios
+            .post(url, formData)
+            .then((res) => {
+                console.log(res)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
     return(
         <React.Fragment>
             <div className="h-auto w-full px-14 pt-28 flex justify-center items-center flex-col">
                 {/* Enter Info */}
                 <div className="bg-[#CD9F9F] w-[80%] h-auto p-10 pt-5 rounded-2xl flex ">
                     <div className="w-[50%] leading-8">
-                        <h3 className="mb-3 font-bold">Thông tin khách hàng</h3>
+                        <h3 className="mb-3 font-bold">Infomation</h3>
                         <div>
-                            <p className="mb-2">Họ và tên</p>
-                            <input className="w-[70%] outline-none px-2"></input>
+                            <p className="mb-2">Full name <span className="text-red-600">*</span></p>
+                            <input 
+                                placeholder="Enter full name"
+                                value={title || ''}
+                                onChange={(event) => {getTitle(event.target.value)}}
+                                className="w-[70%] outline-none rounded px-2"></input>
                         </div>
                         <div>
-                            <p className="mb-2">Ngày sinh</p>
-                            <input className="w-[70%] outline-none px-2 "></input>
+                            <p className="mb-2">Birthday</p>
+                            <input
+                                placeholder="Enter your birthday"
+                                value={birthday || ''}
+                                onChange={(event) => {getBirthday(event.target.value)}} 
+                                className="w-[70%] outline-none rounded px-2"></input>
                         </div>
                         <div>
-                            <p className="mb-2">Số điện thoại</p>
-                            <input className="w-[70%] outline-none px-2"></input>
+                            <p className="mb-2">Phone number <span className="text-red-600">*</span></p>
+                            <input
+                            type="number"
+                            placeholder="Enter Phone Number"
+                            value={phone || ''}
+                            onChange={(event) => {getPhone(event.target.value)}}
+                            className="w-[70%] outline-none rounded px-2"></input>
                         </div>
                         <div>
                             <p className="mb-2">Email</p>
-                            <input className="w-[70%] outline-none px-2"></input>
+                            <input
+                            type="email"
+                            placeholder="Enter email address"
+                            value={email || ''}
+                            onChange={(event) => {getEmail(event.target.value)}}
+                            className="w-[70%] outline-none rounded px-2"></input>
                         </div>
                         <div>
-                            <p className="mb-2">Ghi chú</p>
-                            <textarea className="w-[70%] outline-none px-2"></textarea>
+                            <p className="mb-2">Note</p>
+                            <textarea
+                            placeholder="Type your notes here"
+                            value={note || ''}
+                            onChange={(event) => {getNote(event.target.value)}}
+                            className="w-[70%] outline-none rounded px-2"></textarea>
                         </div>
                     </div>
                     <div className="w-[50%]">
                         {/* Small Calendar */}
-                        <h3 className="mb-2 font-bold">Ngày tháng</h3>
-                        <SmallCalendar />
-
-                        <div>
-                            <h3 className="font-bold mt-3">Thời gian </h3>
+                        <h3 className="mb-2 font-bold">Date<span className="text-red-600">*</span></h3>
+                        {/* <SmallCalendar value={date} onChange={(event) => {getDate(event.target.value)}} /> */}
+                        <Flatpickr
+                            value={date}
+                            options={{
+                                dateFormat: "d-m-Y"
+                            }}
+                            // event
+                            onChange={(dateSelect) => getDate(dateSelect)}
+                        />
+                    <div>
+                            <h3 className="font-bold mt-3">Time <span className="text-red-600">*</span> </h3>
                             <div className="flex justify-between w-[30%]">
-                                <input className="text w-16"></input>
-                                <p>-</p>
-                                <input className="text w-16"></input>
+                                <input
+                                placeholder="Enter full name"
+                                value={startTime || ''}
+                                onChange={(event) => {getTimeStart(event.target.value)}}
+                                className="text w-16 rounded"></input>
+                                <p className="mx-2">to</p>
+                                <input
+                                placeholder="Enter full name"
+                                value={endTime || ''}
+                                onChange={(event) => {getTimeEnd(event.target.value)}}
+                                className="text w-16 rounded"></input>
                             </div>
                         </div>
                     </div>
-                    
-                    
                 </div>
-
                 <div className="my-5">
                     <button className="text-[#CD9F9F] text-2xl rounded-2xl border-2 border-[#CD9F9F] w-[20vh] h-auto py-2 hover:bg-[#CD9F9F] hover:text-white  duration-150 ease-in">Cancel</button>
-                    <button className="text-[#CD9F9F] text-2xl rounded-2xl border-2 border-[#CD9F9F] w-[20vh] h-auto py-2 hover:bg-[#CD9F9F] hover:text-white  duration-150 ease-in ml-10">Book</button>
+                    <button type="submit" onClick={(event) => CreateBooking(event)} className="text-[#CD9F9F] text-2xl rounded-2xl border-2 border-[#CD9F9F] w-[20vh] h-auto py-2 hover:bg-[#CD9F9F] hover:text-white  duration-150 ease-in ml-10">Book</button>
                 </div>
 
-                <div className="w-full flex items-center justify-center">
+                {/* <div className="w-full flex items-center justify-center">
                     <BookingSuccess />
-                </div>
+                </div> */}
             </div>
         </React.Fragment>
     );
