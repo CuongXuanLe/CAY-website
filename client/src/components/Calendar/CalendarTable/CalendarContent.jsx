2c539ab4f9c92f2fb609910c5dd8562c870a93e4
Khaug <huynhkhang690@gmail.com>
2022-12-28 21:22:44 +0700

    [Schedule][865bc92v8] Fix conflict file CalendarContent.jsx

--- client/src/components/Calendar/CalendarTable/CalendarContent.jsx
+++ client/src/components/Calendar/CalendarTable/CalendarContent.jsx
@@ -4,17 +4,9 @@ import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import { Link } from "react-router-dom";
-//import Tippy
-import { tippy } from "@tippyjs/react";
-import 'tippy.js/themes/light.css';
-
+import { Link } from "react-router-dom"
 
 const CalendarContent = () => {
-
-  const [visible, setVisible] = useState(false);
-  const show = () => setVisible(true);
-  const hide = () => setVisible(false);
 
   //getAPI 
   const [schedules, setSchedules] = useState([]);
@@ -39,30 +31,16 @@ const CalendarContent = () => {
               left: 'title',
               right: 'prev,next today'
             }}
-          
-        eventMouseEnter={
-          (arg) => {
-            tippy(arg.el, {
-              content: `<div>${arg.event.title}</div>
-                      <div>${arg.event.start}</div>`,
-              trigger: 'click',
-              allowHTML: true,
-              interactive: true,
-              interactiveBorder: 30,
-              theme: 'light',
-              maxWidth: 500,
-              zIndex: 9999,
-            })
-          }}
         timeZone="none"
         initialView="dayGridMonth"
         navLinks={true}
         selectable={true}
         events={schedules}
       />
+
       <div className="mx-auto">
         <Link to="/booking">
-          <button className="w-15 h-12 rounded-3xl border mt-4 bg-[#a7705c] py-3 px-5 font-bold text-white duration-300 ease-in hover:border-[#a7705c] hover:bg-white hover:text-[#a7705c]">
+          <button className="w-15 h-12 rounded-3xl border-[1px] bg-[#a7705c] py-3 px-5 font-bold text-white duration-300 ease-in hover:border-[#a7705c] hover:bg-white hover:text-[#a7705c]">
             Booking now
           </button>
         </Link>
