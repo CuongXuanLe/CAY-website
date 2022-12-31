25943b870adf22b0a3749434dfc2ea6787b9b081
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-12-31 13:09:51 +0700

    update: modal + [She]del-func

--- admin/src/components/ScheduleContent/ScheduleDetails/ScheduleDetails.jsx
+++ admin/src/components/ScheduleContent/ScheduleDetails/ScheduleDetails.jsx
@@ -1,22 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import moment from "moment";
 const ScheduleDetails = () => {
 
   //getAPI
   const {id} = useParams();
+  const navigate = useNavigate();
   const [infoUsers,setInfoUsers] = useState([]);
+  const newStart = moment(infoUsers.formatted_start, 'DD-MM-YYYY HH:mm');
+  const timeStart = newStart.format('HH:mm');
+  const newEnd = moment(infoUsers.formatted_end, 'DD-MM-YYYY HH:mm');
+  const timeEnd = newEnd.format('HH:mm');
   const infoAPI = `http://127.0.0.1:8000/api/schedule/${id}`;
   useEffect(() => {
     async function getInfo () {
       const res = await axios.get(infoAPI);
       setInfoUsers(res.data)
-      console.log(res.data);
     }
     getInfo();
   },[])
 
+  const deleteBooking = async () => {
+    const res = await axios.delete(infoAPI)
+    .then(({res})=> {
+      console.log('success')
+      alert('delete success')
+      navigate('/schedule')
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+  }
   return (
     <React.Fragment>
       <div className="relative h-full w-full">
@@ -29,8 +44,10 @@ const ScheduleDetails = () => {
           <p className="mt-2">
             <span className="font-bold text-red-600">Appointment Date: </span>{" "}
             {infoUsers.formatted_created_at}{" "}
-            <span className="ml-4 font-bold text-red-600">Time:</span> {infoUsers.formatted_start} to 
-            {" "}{infoUsers.formatted_end}
+            <span className="ml-4 font-bold text-red-600">Time: </span>
+            {timeStart} 
+            <span className="text-red-600 font-bold"> to </span> 
+            {timeEnd}
           </p>
           <p className="mt-2">
             <span className="font-bold">Date of Birth: </span>{infoUsers.formatted_birthday}
@@ -50,11 +67,11 @@ const ScheduleDetails = () => {
         </div>
         <div className="absolute right-1 bottom-5">
           <Link to={`/appointmentedit/${id}`}>
-            <button className="w-20 border-[1px] border-black  duration-150 ease-in hover:bg-black hover:text-white">
+            <button className="w-20 border rounded border-black  duration-150 ease-in hover:bg-black hover:text-white">
               Update
             </button>
           </Link>
-          <button className="ml-4 w-20 border-[1px] border-red-600 text-red-600 duration-150 ease-in hover:bg-red-600 hover:text-white">
+          <button onClick={() => deleteBooking(id)} className="ml-4 w-20 border rounded border-red-600 text-red-600 duration-150 ease-in hover:bg-red-600 hover:text-white">
             Delete
           </button>
         </div>
