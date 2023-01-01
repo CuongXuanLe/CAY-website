3d482e370fd8457717c15fb255428c9e43ccea61
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2023-01-01 18:34:34 +0700

    update: seeder data

--- backend/database/seeders/vp_home.php
+++ backend/database/seeders/vp_home.php
@@ -18,15 +18,15 @@ class vp_home extends Seeder
         DB::table('home')->insert([
             [
                 'title1' => "CREATIVE FORM IN OUR DESIGN",
-                'description1' => "Eg. Our agents are engaged and experienced in the Miami real 
+                'description1' => "Our agents are engaged and experienced in the Miami real
                 estate market and well informed about the communities
                 they services.",
                 'subtitle1' => "CAY STUDIO",
                 'background1' => "https://images.unsplash.com/photo-1574025876844-6c9ba8602866",
-                'description2' => "Eg. A news article discusses current or recent news of either general interest or of a specific toppic tesst2",
+                'description2' => "A news article discusses current or recent news of either general interest or of a specific topic",
                 'background2' => "https://images.unsplash.com/photo-1608324777753-5d2f6e547b1b",
                 'imgAbout' => "https://images.unsplash.com/photo-1608324777753-5d2f6e547b1b",
-                'descriptionAbout' => "Eg. Test Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an"
+                'descriptionAbout' => "Test Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an"
             ],
         ]);
     }
