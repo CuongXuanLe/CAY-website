6ddaa3dbfa8924342f67809d5760a6b84a5d3a23
Khaug <huynhkhang690@gmail.com>
2023-01-01 21:15:12 +0700

    fix conflict ScheduleBooking.jsx

--- client/src/page/Schedule/ScheduleBooking.jsx
+++ client/src/page/Schedule/ScheduleBooking.jsx
@@ -1,13 +1,17 @@
 import React, {useState, useEffect} from "react";
 import { BookingSuccess } from "../../components";
-import { Link } from 'react-router-dom';
-import "flatpickr/dist/flatpickr.css";
-import Flatpickr from "react-flatpickr";
+import { Link } from "react-router-dom";
 import moment from 'moment'
 import axios from "axios";
+//Datepicker
+import Flatpickr from "react-flatpickr";
+import "flatpickr/dist/flatpickr.css";
+//Timepicker ant
+// import { TimePicker } from 'antd';
+
 const ScheduleBooking = () => {
     const [title, getTitle] = useState();
-    const [birthday, getBirthday] = useState();
+    const [birthday, getBirthday] = useState(new Date());
     const [phone, getPhone] = useState();
     const [email, getEmail] = useState();
     const [note, getNote] = useState();
@@ -15,7 +19,9 @@ const ScheduleBooking = () => {
     const [startTime, getTimeStart] = useState();
     const [endTime, getTimeEnd] = useState();
     const [success, setSuccess] = useState();
-    const newDate = moment(date).format('DD-MM-YYYY')
+    const newDate = moment(date).format('DD-MM-YYYY');
+    const newBirthday = moment(birthday).format('DD-MM-YYYY');
+
     const CreateBooking = async (e) => {
         e.preventDefault();
         setSuccess(false);
@@ -23,7 +29,7 @@ const ScheduleBooking = () => {
         const formData = {
             title: title,
             phone: phone,
-            birthday: birthday,
+            birthday: newBirthday,
             email: email,
             note: note,
             created_at: newDate,
@@ -47,7 +53,8 @@ const ScheduleBooking = () => {
     return(
         <React.Fragment>
             <div className="h-auto w-full px-14 pt-28 flex justify-center items-center flex-col">
-                <div className="bg-[#CD9F9F] w-[80%] h-auto p-10 pt-5 rounded-2xl flex ">
+                {/* Enter Info */}
+                <form className="bg-[#CD9F9F] w-[80%] h-auto p-10 pt-5 rounded-2xl flex " onSubmit={CreateBooking}>
                     <div className="w-[50%] leading-8">
                         <h3 className="mb-3 font-bold">Infomation</h3>
                         <div>
@@ -60,11 +67,11 @@ const ScheduleBooking = () => {
                         </div>
                         <div>
                             <p className="mb-2">Birthday</p>
-                            <input
-                                placeholder="Enter your birthday"
-                                value={birthday || ''}
-                                onChange={(event) => {getBirthday(event.target.value)}} 
-                                className="w-[70%] outline-none rounded px-2"></input>
+                            <Flatpickr
+                                value={birthday}
+                                onChange={(selectedDates) => { getBirthday(selectedDates[0]) }}
+                                className="w-[70%] outline-none rounded px-2"
+                            />
                         </div>
                         <div>
                             <p className="mb-2">Phone number <span className="text-red-600">*</span></p>
@@ -98,7 +105,7 @@ const ScheduleBooking = () => {
                         <Flatpickr
                             value={date}
                             onChange={(selectedDates) => {getDate(selectedDates[0])}}
-                            onClick={console.log(date)}
+                            // onClick={console.log(date)}
                         />
                     <div>
                             <h3 className="font-bold mt-3">Time <span className="text-red-600">*</span> </h3>
@@ -107,7 +114,8 @@ const ScheduleBooking = () => {
                                 placeholder="8:30"
                                 value={startTime || ''}
                                 onChange={(event) => {getTimeStart(event.target.value)}}
-                                className="text w-16 px-2 rounded"></input>
+                                className="w-16 px-2 rounded"></input>
+                                
                                 <p className="mx-1">to</p>
                                 <input
                                 placeholder="9:30"
@@ -117,7 +125,7 @@ const ScheduleBooking = () => {
                             </div>
                         </div>
                     </div>
-                </div>
+                </form>
                 <div className="my-5">
                     <Link to="/schedule" >
                         <button className="text-[#CD9F9F] text-2xl rounded-2xl border-2 border-[#CD9F9F] w-[20vh] h-auto py-2 hover:bg-[#CD9F9F] hover:text-white  duration-150 ease-in">Cancel</button>
